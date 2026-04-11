be98fcae20903825685ebaf5541e0e69ccc404db
Lisixo <lisixodev@gmail.com>
2026-04-12 01:14:43 +0200

    feat: update languages

--- src/modules/localization.ts
+++ src/modules/localization.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enLang from "../localization/en.json";
+import enLang from "@/localization/en.json";
 
 i18n.use(initReactI18next).init({
   resources: {

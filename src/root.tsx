6af94743b12d71c96d573267d18b4eaaebc9e3d2
Lisixo <lisixodev@gmail.com>
2026-04-15 00:05:20 +0200

    feat(navbar): separate content from implementation and add floating AI window

--- src/root.tsx
+++ src/root.tsx
@@ -7,8 +7,9 @@ import {
 } from "@tabler/icons-react";
 import {
   isRouteErrorResponse,
-  NavLink,
+  Link,
   Outlet,
+  useLocation,
   useRouteError,
 } from "react-router";
 import { CrashErrorPage, NotFoundErrorPage } from "@/errors";
@@ -19,6 +20,9 @@ import { loaderManager } from "./modules/loader/manager";
 import CSVImporter from "./modules/importers/csv";
 import join from "./utils/join";
 import useProject from "./hooks/useProject";
+import { FloatingPortal, offset, shift, useFloating, useHover, useInteractions } from '@floating-ui/react';
+import { Card, Flex } from "./components/ui/container";
+import { Button } from "./components/ui/button";
 
 export default function Root() {
   const [readyState, setReadyState] = useState<'preparing' | 'ready' | 'fail'>('preparing')
@@ -108,7 +112,7 @@ export default function Root() {
 
       }
       catch(e) {
-        if(import.meta.env.DEV && (e as Error).message.includes("already registered")) {
+        if(import.meta.env.DEV && ((e as Error).message.includes("already registered") || (e as Error).message.includes("Settings are locked"))) {
           console.warn("Error below was captured and ignored in development mode (propably false posivite from StrictMode)")
           console.warn(e)
           return
@@ -156,49 +160,109 @@ export function RootErrorBoundary() {
 }
 
 function NavigationBar() {
+  const isNotLoaded = useProject(s => !s.projectId)
+  const aiEnabled = useSettings(s => s.values["ai.enabled"] as boolean)
+
+  const [AIWindowIsOpen, setAIWindowIsOpen] = useState(false)
+  const {refs, floatingStyles, context} = useFloating({
+    open: AIWindowIsOpen,
+    onOpenChange: setAIWindowIsOpen,
+    middleware: [shift(), offset({ mainAxis: 10, crossAxis: -20 })],
+  });
+
+  const hover = useHover(context, { enabled: aiEnabled, delay: { open: 0, close: 100 }})
+  const {getReferenceProps, getFloatingProps} = useInteractions([hover])
+
+
   return (
-    <nav className="absolute left-0 top-0 w-full h-12 /bg-zinc-800 bg-accent-bg-light px-2 flex justify-between border-b border-b-zinc-500/20">
-      <div className="flex">
-        <NavigationButton href="/">
-          <IconHome />
-        </NavigationButton>
-        <NavigationButton href="/editor" disabled>
-          <IconTools />
-        </NavigationButton>
-        <NavigationButton href="/files" disabled>
-          <IconFolders />
-        </NavigationButton>
-      </div>
-      <div className="flex">
-        <NavigationButton href="/ai">
-          <IconRobotFace />
-        </NavigationButton>
-        <NavigationButton href="/settings">
-          <IconSettings />
-        </NavigationButton>
-      </div>
-    </nav>
+    <>
+      <nav className="absolute left-0 top-0 w-full h-12 /bg-zinc-800 bg-accent-bg-light px-2 flex justify-between border-b border-b-zinc-500/20">
+        <div className="flex">
+          <NavigationButton href="/">
+            <IconHome />
+          </NavigationButton>
+          <NavigationButton href="/editor" disabled={isNotLoaded}>
+            <IconTools />
+          </NavigationButton>
+          <NavigationButton href="/files" disabled={isNotLoaded}>
+            <IconFolders />
+          </NavigationButton>
+        </div>
+        <div className="flex">
+          <NavigationContainer disabled={!aiEnabled} ref={refs.setReference} {...getReferenceProps()}>
+            <IconRobotFace />
+            {
+              (AIWindowIsOpen && aiEnabled) && (
+                <FloatingPortal>
+                  <div style={floatingStyles} ref={refs.setFloating} {...getFloatingProps()}>
+                    <Flex col>
+                      <Card solid>
+                        <Flex col>
+                          <p>AI Status: Disconnected</p>
+                          <p>AI Queue: 0</p>
+                          <Button>
+                            Abort AI Tasks
+                          </Button>
+                        </Flex>
+                      </Card>
+                    </Flex>
+                  </div>
+                </FloatingPortal>
+              )
+            }
+          </NavigationContainer>
+          <NavigationButton href="/settings">
+            <IconSettings />
+          </NavigationButton>
+        </div>
+      </nav>
+    </>
   );
 }
 
-function NavigationButton({ href, children, disabled }: NavigationButtonProps) {
+function NavigationButton({ href, ...rest }: NavigationButtonProps) {  
+  function handleAnchorClick(ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    if(rest.disabled) 
+      ev.preventDefault()
+  }
+
+  const { pathname } = useLocation()
+
+  const endSlashPosition = href !== "/" && href.endsWith("/") ? href.length - 1 : href.length;
+  const isActive = pathname === href || pathname.startsWith(href) && pathname.charAt(endSlashPosition) === "/";
+
   return (
-    <NavLink
+    <Link
       to={href}
-      className={({ isActive }) =>
-        join(
-          `h-full flex p-2 items-center transition-colors`,
-          isActive ? "bg-accent" : disabled ? 'cursor-not-allowed opacity-30' : "hover:bg-accent/20"
-        )
-      }
+      onClick={handleAnchorClick}
     >
-      {children}
-    </NavLink>
+      <NavigationContainer {...rest} isActive={isActive} />
+    </Link>
   );
 }
 
-interface NavigationButtonProps {
+interface NavigationButtonProps extends Omit<NavigationContainerProps, "isActive"> {
   href: string;
+}
+
+interface NavigationContainerProps {
   children?: React.ReactNode;
   disabled?: boolean
+  isActive?: boolean
+}
+
+function NavigationContainer({ children, isActive = false, disabled, ...rest }: NavigationContainerProps & React.HTMLAttributes<HTMLDivElement> & React.ComponentPropsWithRef<'div'>) {
+  return (
+    <div
+      className={
+        join(
+          `h-full flex p-2 items-center transition-colors`,
+          isActive ? "bg-accent" : disabled ? 'cursor-default opacity-30' : "hover:bg-accent/20"
+        )
+      }
+      {...rest}
+    >
+      {children}
+    </div>
+  )
 }
